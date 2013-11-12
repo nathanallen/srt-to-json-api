46d4a8e106c0bb85f31c325fd33512695112cfc8
nathanallen <thenathanator@gmail.com>
2013-11-11 20:18:56 -0800

    mvp api

--- main.rb
+++ main.rb
@@ -1,6 +1,23 @@
 require 'sinatra'
 require "sinatra/json"
 
+
+MY_DATA = {
+	first_name: "Nathan",
+	last_name: "Allen",
+	title: "Junior Web Developer",
+	github: "https://github.com/nathanallen",
+	blog: "http://codeislike.com/",
+	linkedin: "http://www.linkedin.com/in/thisisnathan",
+	twitter: "https://twitter.com/punriddled",
+	email: "first_name + lawrence + last_name@gmail.com",
+}
+
 get '/' do
-	json :msg => "hello world!"
+	json MY_DATA
+end
+
+get '/:detail' do
+	detail = params[:detail].to_sym
+	MY_DATA[detail]
 end
