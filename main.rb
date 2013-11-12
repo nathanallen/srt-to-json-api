222964f8e82e3e7c53e7d1e5656242f8aec2cae6
nathanallen <thenathanator@gmail.com>
2013-11-11 21:20:28 -0800

    add recent tweet logic

--- main.rb
+++ main.rb
@@ -1,6 +1,18 @@
 require 'sinatra'
 require "sinatra/json"
+require 'twitter'
 
+YOUR_CONSUMER_KEY = "ZK8Yb7AHWn5bY3SW2o8Yg"
+YOUR_CONSUMER_SECRET = "16arWqAg5FToVLcuv1sIwmENzrrwcp1hEaDYe2AUKo"
+YOUR_OAUTH_TOKEN = "1482402961-BqN6UbMfy7sUdGHEvEqUzkwvaMS1tfLSRFto0Tl"
+YOUR_OAUTH_TOKEN_SECRET = "bDux66E77MUVgzx0iWDWUzwCYdX2dm4ugYzGn9JVOtW0E"
+
+Twitter.configure do |config|
+  config.consumer_key = YOUR_CONSUMER_KEY
+  config.consumer_secret = YOUR_CONSUMER_SECRET
+  config.oauth_token = YOUR_OAUTH_TOKEN
+  config.oauth_token_secret = YOUR_OAUTH_TOKEN_SECRET
+end
 
 MY_DATA = {
 	first_name: "Nathan",
@@ -11,7 +23,12 @@ MY_DATA = {
 	linkedin: "http://www.linkedin.com/in/thisisnathan",
 	twitter: "https://twitter.com/punriddled",
 	email: "first_name + lawrence + last_name@gmail.com",
+	last_tweet: Twitter.user_timeline("punriddled", count: 1).first[:text]
 }
+
+get '/tweet' do
+	Twitter.user_timeline("punriddled", count: 1).first[:text]
+end
 
 get '/' do
 	json MY_DATA
