3b2d7b207b797af735d6b57e30ab56724dd6755f
nathanallen <thenathanator@gmail.com>
2013-11-12 22:14:54 -0800

    move twitter config into seperate file

--- main.rb
+++ main.rb
@@ -1,21 +1,8 @@
 require 'sinatra'
 require 'sinatra/json'
 require 'twitter'
-require 'yaml'
+require_relative 'twitter_config'
 
-if ENV['RACK_ENV'] == 'development'
-	env_config = YAML.load_file('./env.yaml')
-	env_config.each do |key, value|
-	  ENV[key] = value
-	end
-end
-
-Twitter.configure do |config|
-  config.consumer_key = ENV['TWITTER_KEY']
-  config.consumer_secret = ENV['TWITTER_SECRET']
-  config.oauth_token = ENV['OAUTH_TOKEN']
-  config.oauth_token_secret = ENV['OAUTH_TOKEN_SECRET']
-end
 
 MY_DATA = {
 	contact: {
---
+++ twitter_config.rb
@@ -0,0 +1,15 @@
+require 'yaml'
+
+if ENV['RACK_ENV'] == 'development'
+	env_config = YAML.load_file('secret.yaml')
+	env_config.each do |key, value|
+	  ENV[key] = value
+	end
+end
+
+Twitter.configure do |config|
+  config.consumer_key = ENV['TWITTER_KEY']
+  config.consumer_secret = ENV['TWITTER_SECRET']
+  config.oauth_token = ENV['OAUTH_TOKEN']
+  config.oauth_token_secret = ENV['OAUTH_TOKEN_SECRET']
+end
