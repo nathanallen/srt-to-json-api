3e82bf4e378d65fe038d8f7660700a39958d32cf
nathanallen <thenathanator@gmail.com>
2013-11-13 10:00:57 -0800

    tweak api data

--- api_data.rb
+++ api_data.rb
@@ -1,24 +1,23 @@
 API_DATA = {
 	directory: {
 		contact_url: "http://api.codeislike.com/contact",
-		contact_email_url: "http://api.codeislike.com/contact/email",
 		status_url: "http://api.codeislike.com/status",
-		twitter_status_url: "http://api.codeislike.com/status/last_tweet"
+		twitter_status_url: "http://api.codeislike.com/status/twitter"
 	},
 	contact: {
 		first_name: "Nathan",
 		middle_name: "Lawrence",
 		last_name: "Allen",
 		job_title: "Junior Web Developer",
-		github: "https://github.com/nathanallen",
-		blog: "http://codeislike.com/",
-		linkedin: "http://www.linkedin.com/in/thisisnathan",
-		twitter: "https://twitter.com/punriddled",
-		email: "first_name + middle_name + last_name + '@gmail.com'",
+		github_url: "https://github.com/nathanallen",
+		blog_url: "http://codeislike.com/",
+		linkedin_url: "http://www.linkedin.com/in/thisisnathan",
+		twitter_url: "https://twitter.com/punriddled",
+		email: "first_name + middle_name + last_name + '@gmail.com'"
 	},
 	status: {
 		awake: (Time.now.hour > 8 && Time.now.hour < 23),
 		job_status: "Actively looking",
-		last_tweet: Twitter.user_timeline("punriddled", count: 1).first[:text],
+		twitter: Twitter.user_timeline("punriddled", count: 1).first[:text]
 	},
 }
