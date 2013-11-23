17294ffd1c93cfc8fe7757cb3408155306e18611
nathanallen <thenathanator@gmail.com>
2013-11-23 14:54:47 -0800

    test curl method with hardcoded url

--- main.rb
+++ main.rb
@@ -4,3 +4,7 @@ require 'sinatra/json'
 before do
 	response.header['Access-Control-Allow-Origin'] = '*'
 end
+
+get '/' do
+ `curl http://new.outloudradio.org/sites/default/files/transcripts/Asexuality_FAQ.en_.srt`
+end
