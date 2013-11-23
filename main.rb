4c09067e04dc8bcfb078402b504eb769584d249a
nathanallen <thenathanator@gmail.com>
2013-11-23 14:32:01 -0800

    starting from a clean slate

--- main.rb
+++ main.rb
@@ -1,18 +1,6 @@
 require 'sinatra'
 require 'sinatra/json'
-require_relative 'twitter_config'
-require_relative 'api_data'
 
 before do
 	response.header['Access-Control-Allow-Origin'] = '*'
-end
-
-get '/' do
-	json API_DATA[:directory]
-end
-
-get '/:category/?*' do
-	category = params[:captures][0].to_sym
-	item = params[:captures][1].to_sym unless params[:captures][1] == ""
-	item ? json(API_DATA[category][item]) : json(API_DATA[category])
 end
